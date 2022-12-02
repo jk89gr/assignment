cf91a7c74fca0822649a6210a8922f9587cf6403
Dimitris Koutsodimos <jk89gr@gmail.com>
2022-12-02 17:59:21 +0200

    Add updateCompany req

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,34 @@
 const baseURL = "https://dev-api.inop.ai/api/v1/dev/companies";
 
+/**
+ * Fetch companies
+ *
+ * @returns json object
+ */
 export const getCompanies = async () => {
-    try {
-        const res = await fetch(baseURL);
-        const json = await res.json();
-        console.debug("getCompanies");
-        return json;
-    } catch (error) {
-        console.error(error);
+  try {
+    const res = await fetch(baseURL);
+    const json = await res.json();
+    console.debug("getCompanies");
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+/**
+ * Update a company
+ *
+ * @param {string} id the company's id.
+ * @param {json} data the payload.
+ */
+export const updateCompany = (id, data) => {
+  console.debug("updateCompany");
+  return fetch(`${baseURL}/${id}`, {
+    method: 'PATCH',
+    body: data,
+    headers: {
+      "Accept": "application/json"
     }
+  });
 };
